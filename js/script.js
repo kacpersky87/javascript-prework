0cd7b1df039c74f28d139b28a32e66a36c3179f5
Kacper Łukaszczyk <k.lukaszczyk@gmail.com>
2023-03-31 15:03:23 +0200

    Add randomNumer else if for papier & nożyce

--- js/script.js
+++ js/script.js
@@ -1,7 +1,33 @@
 printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
-var computerMove;
+var computerMove, randomNumber;
+randomNumber = Math.floor(math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+    computerMove = 'Kamień';
+} else {
+    computerMove = 'Nieznany ruch';
+}
+if (randomNumber == '2') {
+    computerMove = 'Papier';
+} else {
+    computerMove = 'Nieznany ruch';
+}
+if (randomNumber == '3') {
+    computerMove = 'Nożyce';
+} else {
+    computerMove = 'Nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
+
+
+
+
+
 computerMove = 'kamień';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 var playerMove;
 playerMove = 'papier';
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to wygrywam!');
+var randomNumber;
+randomNumber = Math.floor(math.random() * 3 + 1);
+printMessage('Wylosowana liczba to: ' + randomNumber);
