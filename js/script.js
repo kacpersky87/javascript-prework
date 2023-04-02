90c89f14fda0f43fd05ae9a56aca2b8445685c76
Kacper Łukaszczyk <k.lukaszczyk@gmail.com>
2023-04-02 18:04:00 +0200

    Add styles changes

--- js/script.js
+++ js/script.js
@@ -51,7 +51,7 @@ function displayResult(argPlayerMove, argComputerMove) {
   } else if (argPlayerMove == argComputerMove) {
     printMessage('Remis!');
   } else {
-    printMessage('Przegrywasz :(');
+    printMessage('Przegrywasz...');
   }
   printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 }
